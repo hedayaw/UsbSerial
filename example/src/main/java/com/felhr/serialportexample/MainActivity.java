6c3f5bf4e963f4ad3cfc85384b779b98f06a868c
hedayaw <41341258+hedayaw@users.noreply.github.com>
2019-03-11 14:29:49 -0400

    Updated MainActivity.java
    added my tested code for displaying the accelerometer and gyroscope data

--- example/src/main/java/com/felhr/serialportexample/MainActivity.java
+++ example/src/main/java/com/felhr/serialportexample/MainActivity.java
@@ -158,19 +158,117 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (!editText.getText().toString().equals("")) {
                     String data = editText.getText().toString();
-                    if (usbService != null) { // if UsbService was correctly binded, Send data
-                        usbService.write(data.getBytes());
-                    }
+                if (usbService != null) { // if UsbService was correctly binded, Send data
+                    usbService.write(data.getBytes());
+                }
                 }
             }
         });
     }
 
     @Override
+	   public void onSensorChanged(SensorEvent sensorEvent) {
+		     Sensor mySensor = sensorEvent.sensor;
+
+		if (startData) {
+			if ((mySensor.getType() == Sensor.TYPE_ACCELEROMETER)) {
+				float x = sensorEvent.values[0];
+				float y = sensorEvent.values[1];
+				float z = sensorEvent.values[2];
+
+				String x_valueA = String.valueOf(x);
+				String y_valueA = String.valueOf(y);
+				String z_valueA = String.valueOf(z);
+
+				final TextView xStringValueA = findViewById(R.id.textView5);
+				xStringValueA.setText(x_valueA);
+
+				final TextView yStringValueA = findViewById(R.id.textView6);
+				yStringValueA.setText(y_valueA);
+
+				final TextView zStringValueA = findViewById(R.id.textView7);
+				zStringValueA.setText(z_valueA);
+
+				sendCommand(x_valueA, y_valueA, z_valueA);
+			}
+
+			if (timestamp != 0) {
+				final float dT = (sensorEvent.timestamp - timestamp) * NS2S;
+				float axisX = sensorEvent.values[0];
+				float axisY = sensorEvent.values[1];
+				float axisZ = sensorEvent.values[2];
+
+				String x_valueG = String.valueOf(axisX);
+				String y_valueG = String.valueOf(axisY);
+				String z_valueG = String.valueOf(axisZ);
+
+				final TextView xStringValue = findViewById(R.id.textView14);
+				xStringValue.setText(x_valueG);
+
+				final TextView yStringValue = findViewById(R.id.textView15);
+				yStringValue.setText(y_valueG);
+
+				final TextView zStringValue = findViewById(R.id.textView16);
+				zStringValue.setText(z_valueG);
+
+				sendCommand(x_valueG, y_valueG, z_valueG);
+
+				float omegaMagnitude = (float) Math.sqrt((axisX * axisX) + (axisY * axisY) + (axisZ * axisZ));
+
+				float thetaOverTwo = omegaMagnitude * dT / 2.0f;
+				float sinThetaOverTwo = (float) Math.sin(thetaOverTwo);
+				float cosThetaOverTwo = (float) Math.cos(thetaOverTwo);
+				deltaRotationVector[0] = sinThetaOverTwo * axisX;
+				deltaRotationVector[1] = sinThetaOverTwo * axisY;
+				deltaRotationVector[2] = sinThetaOverTwo * axisZ;
+				deltaRotationVector[3] = cosThetaOverTwo;
+			}
+
+			timestamp = sensorEvent.timestamp;
+			float[] deltaRotationMatrix = new float[9];
+			SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
+
+		} else {
+			final TextView xStringValueA = findViewById(R.id.textView5);
+			xStringValueA.setText("--");
+
+			final TextView yStringValueA = findViewById(R.id.textView6);
+			yStringValueA.setText("--");
+
+			final TextView zStringValueA = findViewById(R.id.textView7);
+			zStringValueA.setText("--");
+
+			final TextView xStringValue = findViewById(R.id.textView14);
+			xStringValue.setText("--");
+
+			final TextView yStringValue = findViewById(R.id.textView15);
+			yStringValue.setText("--");
+
+			final TextView zStringValue = findViewById(R.id.textView16);
+			zStringValue.setText("--");
+		}
+	}
+
+    @Override
     public void onResume() {
         super.onResume();
         setFilters();  // Start listening notifications from UsbService
         startService(UsbService.class, usbConnection, null); // Start UsbService(if it was not started before) and Bind it
+
+		    sensorManagerA.registerListener(this, sensorA, SensorManager.SENSOR_DELAY_NORMAL);
+		    sensorManagerG.registerListener(this, sensorG, SensorManager.SENSOR_DELAY_NORMAL);
+
+		    Intent intent = getIntent();
+		    String action = intent.getAction();
+
+		    UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+		    if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)) {
+			     setDevice(device);
+		    } else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
+			       if (deviceFound != null && deviceFound.equals(device)) {
+				         setDevice(null);
+			       }
+		    }
     }
 
     @Override
@@ -178,7 +276,87 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         unregisterReceiver(mUsbReceiver);
         unbindService(usbConnection);
+        sensorManagerA.unregisterListener(this);
+		    sensorManagerG.unregisterListener(this);
     }
+
+    private void setDevice(UsbDevice device) {
+		usbInterfaceFound = null;
+		endpointOut = null;
+		endpointIn = null;
+
+		for (int i = 0; i < device.getInterfaceCount(); i++) {
+			UsbInterface usbif = device.getInterface(i);
+
+			UsbEndpoint tOut = null;
+			UsbEndpoint tIn = null;
+
+			int tEndpointCnt = usbif.getEndpointCount();
+			if (tEndpointCnt >= 2) {
+				for (int j = 0; j < tEndpointCnt; j++) {
+					if (usbif.getEndpoint(j).getType() == UsbConstants.USB_ENDPOINT_XFER_BULK) {
+						if (usbif.getEndpoint(j).getDirection() == UsbConstants.USB_DIR_OUT) {
+							tOut = usbif.getEndpoint(j);
+						} else if (usbif.getEndpoint(j).getDirection() == UsbConstants.USB_DIR_IN) {
+							tIn = usbif.getEndpoint(j);
+						}
+					}
+				}
+
+				if (tOut != null && tIn != null) {
+					usbInterfaceFound = usbif;
+					endpointOut = tOut;
+					endpointIn = tIn;
+				}
+			}
+		}
+
+		if (usbInterfaceFound == null) {
+			return;
+		}
+
+		deviceFound = device;
+
+		if (device != null) {
+			UsbDeviceConnection connection = usbManager.openDevice(device);
+			if (connection != null && connection.claimInterface(usbInterfaceFound, true)) {
+				usbDeviceConnection = connection;
+				Thread thread = new Thread((Runnable) this); //WHY
+				thread.start();
+			} else {
+				usbDeviceConnection = null;
+			}
+		}
+	}
+
+	private void sendCommand(String x, String y, String z) { //(int control)
+		synchronized (this) {
+			if (usbDeviceConnection != null) {
+				String data = x + " " + y + " " + z;
+				byte[] message = data.getBytes();
+//				message[0] = (byte)control;
+
+				usbDeviceConnection.bulkTransfer(endpointOut, message, message.length, 0);
+			}
+		}
+	}
+
+	public void run() {
+		ByteBuffer buffer = ByteBuffer.allocate(1);
+		UsbRequest request = new UsbRequest();
+		request.initialize(usbDeviceConnection, endpointIn);
+		while (true) {
+			request.queue(buffer, 1);
+			if(usbDeviceConnection.requestWait() == request) {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+				}
+			} else {
+				break;
+			}
+		}
+	}
 
     private void startService(Class<?> service, ServiceConnection serviceConnection, Bundle extras) {
         if (!UsbService.SERVICE_CONNECTED) {
